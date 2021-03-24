cad8e9d728ef40e7dde2b9d84d33fb2fb4d1ce0d
Ramie Mosely <ramiemosely5@gmail.com>
2021-03-24 16:14:37 -0400

    Begun work on gameplay mechanics

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const grid = document.querySelector('.grid');
+const resultsDisplay = document.querySelector('.results')
 let currentShooterIndex = 202;
 let width = 15
+let direction = 1
+let invadersId
+let goingRight = true
 
 for(let i = 0; i < 225; i++)
 {
@@ -26,6 +30,14 @@ function draw()
 
 draw()
 
+function remove()
+{
+    for(let i = 0; i < alienInvaders.length; i++)
+    {
+        squares[alienInvaders[i]].classList.remove('invader')
+    }
+}
+
 squares[currentShooterIndex].classList.add('shooter')
 
 function moveShooter(e) {
@@ -46,4 +58,52 @@ document.addEventListener('keydown', moveShooter)
 function moveInvaders()
 {
     const leftEdge = alienInvaders[0] % width === 0
+    const rightEdge = alienInvaders[alienInvaders.length - 1] % width === width -1
+    remove()
+
+    if(rightEdge && goingRight) 
+    {
+        for(let i = 0; i < alienInvaders.length; i++)
+        {
+            alienInvaders[i] += width +1
+            direction = -1
+            goingRight = false
+
+        }
+    }
+
+    if(leftEdge && !goingRight)
+    {
+        for(let i = 0; i < alienInvaders.length; i++)
+        {
+            alienInvaders[i] += width -1
+            direction = 1
+            goingRight = true
+        }
+    }
+
+    for (let i = 0; i < alienInvaders.length; i++)
+    {
+        alienInvaders[i] += direction
+    }
+    draw()
+
+    if(square[currentShooterIndex].classList.contains('invader', 'shooter'))
+    {
+        resultsDisplay.innerHTML = 'GAME OVER'
+        clearInterval(invadersId)
+    }
+
+    for(let i = 0;i < alienInvaders.length; i++)
+    {
+        if(alienInvaders[i] > squares.length)
+        {
+            resultsDisplay.innerHTML = 'GAME OVER'
+        }
+
+    }
+
+
 }
+
+invadersId = setInterval(moveInvaders, 500)
