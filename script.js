1c864ad5b77e500b205d348d5c6bf61e02d58b22
Ramie Mosely <ramiemosely5@gmail.com>
2021-03-26 13:28:40 -0400

    Completed laser for player

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let width = 15
 let direction = 1
 let invadersId
 let goingRight = true
+let alienRemoved
 
 for(let i = 0; i < 225; i++)
 {
@@ -88,7 +89,7 @@ function moveInvaders()
     }
     draw()
 
-    if(square[currentShooterIndex].classList.contains('invader', 'shooter'))
+    if(squares[currentShooterIndex].classList.contains('invader', 'shooter'))
     {
         resultsDisplay.innerHTML = 'GAME OVER'
         clearInterval(invadersId)
@@ -99,11 +100,43 @@ function moveInvaders()
         if(alienInvaders[i] > squares.length)
         {
             resultsDisplay.innerHTML = 'GAME OVER'
+            clearInterval(invadersId)
         }
-
     }
-
-
 }
 
 invadersId = setInterval(moveInvaders, 500)
+
+function shoot(e)
+{
+    let laserId
+    let currentLaserIndex = currentShooterIndex
+
+    function moveLaser()
+    {
+        squares[currentLaserIndex].classList.remove('laser')
+        currentLaserIndex -= width
+        squares[currentLaserIndex].classList.add('laser')
+
+        if(squares[currentLaserIndex].classList.contains('invader'))
+        {
+            squares[currentLaserIndex].classList.remove('laser')
+            squares[currentLaserIndex].classList.remove('invader')
+            squares[currentLaserIndex].classList.add('boom')
+
+            setTimeout(()=> squares[currentLaserIndex].classList.remove('boom'), 300)
+            clearInterval(laserId)
+
+            const alienRemoval = alienInvaders.indexOf(currentLaserIndex)
+
+            alienInvaders
+        }
+    }
+    switch(e.key)
+    {
+        case 'ArrowUp':
+        laserId = setInterval(moveLaser, 100)
+    }
+}
+
+document.addEventListener('keydown', shoot)
